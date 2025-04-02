170bc3160bb3aa951d0a4fc5bab1fe659f12e92f
moonstone0514 <comengiee852@naver.com>
2025-04-02 17:10:06 +0900

    security 부서 호스트 생성 및 네트워크 생성, 호스트 생성 스크립트, 호스트 삭제 스크립트 생성

--- 25-1captstone/docker/container_control/Security_department/security10/1-create_security10.sh
+++ 25-1captstone/docker/container_control/Security_department/security10/1-create_security10.sh
@@ -10,4 +10,4 @@ docker start security10;
 #앞단에 스위치를 둘것인지 아니면 바로 다른 컨테이너와 연결되게끔 할것인지
 #docker network connect (네트워크명) --ip (상대 IP) security01;
 
-docker network connect --ip 172.21.10.11 security_net security10
+docker network connect --ip 172.21.10.12 security_net security10
