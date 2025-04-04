56cdb6864e83d31a4cd7b8cfe75dfe4c31553bca
KMS0514 <comengiee852.mk@gmail.com>
2025-04-05 02:16:19 +0900

    elasticsearch, kibana, splunk 각각 포트 포워딩 명령어 스크립트 생성

---
+++ 25-1captstone/kubernetes/container_control/service/services/elasticsearch_portforward.sh
@@ -0,0 +1,3 @@
+echo "[+] Elasticsearch 포트포워딩 시작 (9200)"
+kubectl port-forward pod/$(kubectl get pod -l app=elasticsearch -o jsonpath="{.items[0].metadata.name}") 9200:9200 >/dev/null 2>&1 &
+echo "→ http://localhost:9200"
---
+++ 25-1captstone/kubernetes/container_control/service/services/splunk_portforward.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "[+] Splunk 포트포워딩 시작 (8000)"
+kubectl port-forward pod/$(kubectl get pod -l app=splunk -o jsonpath="{.items[0].metadata.name}") 8000:8000 >/dev/null 2>&1 &
+echo "→ http://localhost:8000"
